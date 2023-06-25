669ba8e60f1d61249af4e8d063d403ef2762e1ee
Jon Chang <jonasenchang@gmail.com>
2023-06-25 09:36:41 -0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jon Chang on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
